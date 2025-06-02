0c71804c26f6d8ea286219bb48de5958cf2db46a
Mustafa CAVUS <45355486+mcavs@users.noreply.github.com>
2025-06-02 22:53:14 +0300

    Update survival_dropout.R

--- survival_dropout.R
+++ survival_dropout.R
@@ -1,5 +1,18 @@
+---
+title: "Survival Student Dropout Models"
+output: pdf_document
+date: 
+---
+
+```{r setup, include=FALSE}
+knitr::opts_chunk$set(echo = TRUE)
+```
+
+# Packages
+
+```{r eval=FALSE, include=FALSE}
 # packages
-install.packages(c("mlr3", "mlr3proba", "mlr3learners", "mlr3measures", "mlr3verse", "survival", "randomForestSRC", "devtools", "dplyr", "lubridate"))
+#install.packages(c("mlr3", "mlr3proba", "mlr3learners", "mlr3measures", "mlr3verse", "survival", "randomForestSRC", "devtools", "dplyr", "lubridate"))
 library(devtools)
 devtools::install_github("jakubkuzilek/oulad")
 library(oulad)
@@ -12,16 +25,22 @@ library(mlr3proba)
 library(mlr3learners)
 library(mlr3measures)
 library(mlr3verse) 
+library(stringr)
+```
 
+
+# Dataset
+
+```{r}
 # survival data with demographics
 surv_data <- oulad::student |>
   left_join(oulad::student_registration, 
             by = c("id_student", "code_module", "code_presentation")) |>
-  filter(code_module       == "AAA", 
-         code_presentation == "2013J") |>
-  mutate(time  = date_unregistration - date_registration,
-         time  = ifelse(is.na(time), 269 - date_registration, time),
-         event = ifelse(is.na(date_unregistration), 0, 1))
+  filter(code_module       == "GGG") |>
+  filter(str_detect(code_presentation, "J")) |>
+  mutate(time  = ifelse(is.na(date_unregistration), 269, date_unregistration),
+         event = ifelse(is.na(date_unregistration), 0, 1)) |>
+  filter(time > 0)
 
 # survival data with vle
 vle_agg <- oulad::student_vle |>
@@ -35,14 +54,15 @@ vle_agg <- oulad::student_vle |>
 surv_data <- surv_data |>
   left_join(vle_agg, by = c("id_student", "code_module", "code_presentation")) |>
   mutate(prior_clicks = replace_na(prior_clicks, 0))
+```
 
-# Cox model training
-cox_model <- coxph(Surv(time, event) ~ age_band + gender + highest_education + imd_band + prior_clicks,
-                   data = surv_data)
 
-# Random survival forest model training
+# Preparing dataset
+
+```{r}
 surv_data_rf <- surv_data |>
-  select(gender, region, highest_education, imd_band, age_band, disability, prior_clicks, time, event) |>
+  select(gender, region, highest_education, imd_band, age_band,
+         disability, prior_clicks, time, event) |>
   mutate(
     age_band          = as.factor(age_band),
     gender            = as.factor(gender),
@@ -50,25 +70,52 @@ surv_data_rf <- surv_data |>
     imd_band          = as.factor(imd_band),
     region            = as.factor(region)) |>
   na.omit()
+```
 
+# Cox PH assumption
+
+```{r eval=FALSE, include=FALSE}
 task <- TaskSurv$new(
   id      = "oulad_surv",
   backend = surv_data_rf,
   time    = "time",
   event   = "event")
 
-learner_cox <- lrn("surv.coxph")
-learner_rsf <- lrn("surv.rfsrc")
+learner <- lrn("surv.coxph")
+
+learner$train(task)
+
+cox.zph(learner$model)
+```
 
 
-resampling <- rsmp("cv", folds = 10)
+# Setup
+
+```{r eval=FALSE, include=FALSE}
+task <- TaskSurv$new(
+  id      = "oulad_surv",
+  backend = surv_data_rf,
+  time    = "time",
+  event   = "event")
+
+learners <- c(lrn("surv.coxph"), lrn("surv.rfsrc",
+                                     ntree    = 476,
+                                     mtry     = 2,
+                                     nodesize = 15))
+
+resampling <- rsmp("cv", folds = 5)
 resampling$instantiate(task)
 
 measures <- list(
   msr("surv.brier"),  
   msr("surv.cindex")                        
 )
+```
 
+
+# Model performance
+
+```{r eval=FALSE, include=FALSE}
 bmr <- benchmark(
   benchmark_grid(
     tasks       = task,
@@ -78,8 +125,47 @@ bmr <- benchmark(
 )
 
 bmr$aggregate(measures)
+```
 
 
+# Hyperparameter tuning
 
+```{r echo=TRUE}
+library(paradox)
+library(mlr3tuning)
 
+task <- TaskSurv$new(
+  id      = "oulad_surv",
+  backend = surv_data_rf,
+  time    = "time",
+  event   = "event")
 
+measure <- msr("surv.cindex")
+
+resampling <- rsmp("cv", folds = 5)
+resampling$instantiate(task)
+
+param_set <- ps(
+  ntree     = p_int(lower = 100, upper = 500),
+  mtry      = p_int(lower = 2,   upper = 6),
+  nodesize  = p_int(lower = 5,   upper = 25)
+)
+
+tuner <- tnr("random_search")  
+terminator <- trm("evals", n_evals = 20)  
+
+learner <- lrn("surv.rfsrc")
+at <- AutoTuner$new(
+  learner      = learner,
+  resampling   = resampling,
+  measure      = measure,
+  search_space = param_set,
+  terminator   = terminator,
+  tuner        = tuner
+)
+
+at$train(task)
+at$learner$model 
+at$archive$data   
+
+```
